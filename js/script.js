2b82761be0fe319ec6137f95be6707a39dfa062c
wissam1234 <wissam.ghozlan@gmail.com>
2016-11-07 15:10:20 +0100

    new version

---
+++ js/script.js
@@ -0,0 +1,82 @@
+/*audio player*/    
+function play(sound){
+ var audio = document.getElementById("audio");
+ audio.setAttribute('src', sound);
+ audio.play();
+}
+function stopAudio(){
+ var audio = document.getElementById("audio");
+ audio.pause();
+}
+
+// Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+      target: '.navbar-fixed-top',
+      offset: 51
+    });
+
+/*swip for mobile*/
+    $(document).ready(function() {  
+
+          //Enable swiping...
+          $(".carousel-inner").swipe( {
+            //Generic swipe handler for all directions
+            swipeLeft:function(event, direction, distance, duration, fingerCount) {
+              $(this).parent().carousel('next'); 
+            },
+            swipeRight: function() {
+              $(this).parent().carousel('prev'); 
+            },
+            //Default is 75px, set to 0 for demo so any distance triggers swipe
+            threshold:0
+          });
+        });
+
+/*scroll down*/ 
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').stop().animate({
+          scrollTop: target.offset().top 
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+//scroll to top button
+$(document).ready(function(){
+     $(window).scroll(function () {
+            if ($(this).scrollTop() > 50) {
+                $('#back-to-top').fadeIn();
+            } else {
+                $('#back-to-top').fadeOut();
+            }
+        });
+        // scroll body to 0px on click
+        $('#back-to-top').click(function () {
+            $('#back-to-top').tooltip('hide');
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+        
+        $('#back-to-top').tooltip('show');
+
+});
+
+//analytics.google
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-84707039-1', 'auto');
+  ga('send', 'pageview');
+
+
