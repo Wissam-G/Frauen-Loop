ddeca4814f83e061aafc9891404d199912cfb74e
wissam1234 <wissam.ghozlan@gmail.com>
2016-12-15 00:35:44 +0100

    donate page test

--- js/script.js
+++ js/script.js
@@ -70,6 +70,12 @@ $(document).ready(function(){
 
 });
 
+$(document).ready(function () {
+
+    $(".player").mb_YTPlayer();
+
+});
+
 //analytics.google
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
