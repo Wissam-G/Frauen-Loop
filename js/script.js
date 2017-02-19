3ab499d725edda4558d84710c37f8fd04921b758
wissam1234 <wissam.ghozlan@gmail.com>
2017-02-19 14:24:54 +0100

    add image carousel + delete LISTEN page

--- js/script.js
+++ js/script.js
@@ -10,13 +10,13 @@ function stopAudio(){
 }
 
 // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-      target: '.navbar-fixed-top',
-      offset: 51
-    });
+$('body').scrollspy({
+  target: '.navbar-fixed-top',
+  offset: 51
+});
 
 /*swip for mobile*/
-    $(document).ready(function() {  
+$(document).ready(function() {  
 
           //Enable swiping...
           $(".carousel-inner").swipe( {
@@ -50,61 +50,61 @@ $(function() {
 
 //scroll to top button
 $(document).ready(function(){
-     $(window).scroll(function () {
-            if ($(this).scrollTop() > 50) {
-                $('#back-to-top').fadeIn();
-            } else {
-                $('#back-to-top').fadeOut();
-            }
-        });
+ $(window).scroll(function () {
+  if ($(this).scrollTop() > 50) {
+    $('#back-to-top').fadeIn();
+  } else {
+    $('#back-to-top').fadeOut();
+  }
+});
         // scroll body to 0px on click
         $('#back-to-top').click(function () {
-            $('#back-to-top').tooltip('hide');
-            $('body,html').animate({
-                scrollTop: 0
-            }, 800);
-            return false;
+          $('#back-to-top').tooltip('hide');
+          $('body,html').animate({
+            scrollTop: 0
+          }, 800);
+          return false;
         });
         
         $('#back-to-top').tooltip('show');
 
-});
+      });
 
 $(document).ready(function () {
 
-    $(".player").mb_YTPlayer();
+  $(".player").mb_YTPlayer();
 
 });
 
 //analytics.google
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-84707039-1', 'auto');
-  ga('send', 'pageview');
+ga('create', 'UA-84707039-1', 'auto');
+ga('send', 'pageview');
 
 
 //check out Strip.com
-        $('#stripe-button').click(function(){
-          var token = function(res){
-            var $id = $('<input type=hidden name=stripeToken />').val(res.id);
-            var $email = $('<input type=hidden name=stripeEmail />').val(res.email);
-            $('form').append($id).append($email).submit();
-          };
+$('#stripe-button').click(function(){
+  var token = function(res){
+    var $id = $('<input type=hidden name=stripeToken />').val(res.id);
+    var $email = $('<input type=hidden name=stripeEmail />').val(res.email);
+    $('form').append($id).append($email).submit();
+  };
 
-          var amount = $("#stripeAmount").val();
-          StripeCheckout.open({
-            key:         'pk_test_6pRNASCoBOKtIshFeQd4XMUh',
-            amount:      '1000',
-            name:        'FrauenLoop gUG',
-            image:       'https://s3.amazonaws.com/stripe-uploads/acct_19OnzhHR0Wgl5Lq7merchant-icon-1481288958924-Logo_Frauenloop_rot_web_small.jpg',
-            description: 'Widget',
-            currency:    'eur'
-          });
+  var amount = $("#stripeAmount").val();
+  StripeCheckout.open({
+    key:         'pk_test_6pRNASCoBOKtIshFeQd4XMUh',
+    amount:      '1000',
+    name:        'FrauenLoop gUG',
+    image:       'https://s3.amazonaws.com/stripe-uploads/acct_19OnzhHR0Wgl5Lq7merchant-icon-1481288958924-Logo_Frauenloop_rot_web_small.jpg',
+    description: 'Widget',
+    currency:    'eur'
+  });
 
-          return false;
-        });
+  return false;
+});
 
-        
+
